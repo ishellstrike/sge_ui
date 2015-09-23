9a605fd94c8bd07cec5b48c880ec1384def93dc3
af.samsonov <af.samsonov@tensor.ru>
2015-09-23 19:49:59 +0300

    treev

--- treeview.cpp
+++ treeview.cpp
@@ -10,9 +10,27 @@
 #include "../mouse.h"
 #include "../helper.h"
 
+#include <boost/graph/graph_traits.hpp>
+#include <sge_ui/label.h>
+
 TreeView::TreeView(WContainer *par) :
-    WComponent(par)
+    WContainer(par)
 {
+    g = std::make_shared<Graph>(1);
+    auto v = std::make_shared<Vertex>(g, stored);
+    stored.push_back(v);
+
+    auto ll = new Label(this);
+    v->add_child(ll);
+
+    ll = new Label(this);
+    auto out = v->add_child(ll);
+
+    ll = new Label(this);
+    out->add_child(ll);
+
+    ll = new Label(this);
+    out->add_child(ll);
 }
 
 TreeView::~TreeView()
@@ -30,6 +48,13 @@ void TreeView::Draw() const
 
     WinS::sb->drawText(text, pos, size, WinS::f, aimed ? WinS::color.hovered_text : WinS::color.text, SpriteBatch::ALIGN_CENTER);
 
+    typedef boost::graph_traits<Graph>::vertex_iterator vertex_iter;
+    std::pair<vertex_iter, vertex_iter> vp;
+    for (vp = boost::vertices(*g); vp.first != vp.second; ++vp.first)
+    {
+        LOG(info) << *vp.first << " " << *vp.second;
+    }
+
     WComponent::Draw();
 }
 
@@ -38,3 +63,23 @@ void TreeView::Update(const GameTimer &gt, const MouseState &ms)
     WComponent::Update(gt, ms);
 }
 
+
+
+TreeView::Vertex::Vertex(std::shared_ptr<TreeView::Graph> &__gr, std::vector<std::shared_ptr<Vertex> > &__stored) : gr(__gr), stored(__stored)
+{
+
+}
+
+std::shared_ptr<TreeView::Vertex> TreeView::Vertex::add_child(WComponent *wc)
+{
+    int post_last = stored.size() - 1;
+    auto a = std::make_shared<Vertex>(gr, stored);
+    a->store = wc;
+    index = post_last;
+
+    stored.push_back(a);
+    auto n = boost::add_vertex(*gr);
+    boost::add_edge(index, n, *gr);
+
+    return stored[post_last];
+}
--- treeview.h
+++ treeview.h
@@ -10,11 +10,33 @@
 #include <glm/glm.hpp>
 #include <functional>
 
-class TreeView : public WComponent
+#include <boost/graph/adjacency_list.hpp>
+
+class TreeView : public WContainer
 {
 public:
     TreeView(WContainer *par);
     ~TreeView();
+
+    struct Vertex;
+
+    typedef std::pair<int,int> E;
+    typedef boost::adjacency_list<> Graph;
+
+    struct Vertex
+    {
+        WComponent *store;
+        int index = 0;
+
+        std::shared_ptr<Graph> &gr;
+        std::vector<std::shared_ptr<Vertex>> &stored;
+
+        Vertex(std::shared_ptr<Graph> &__gr, std::vector<std::shared_ptr<Vertex>> &__stored);
+        std::shared_ptr<TreeView::Vertex> add_child(WComponent *wc);
+    };
+
+    std::shared_ptr<Graph> g;
+    std::vector<std::shared_ptr<Vertex>> stored;
 
     void Draw() const override;
     void Update(const GameTimer &gt, const MouseState &ms) override;
